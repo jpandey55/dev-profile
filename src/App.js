42fc6d4f608b6188e910149bf7e080d548f14f3c
Jitendra Kumar Pandey <pandeyjitendra5994@gmail.com>
2024-11-05 21:18:45 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <div>
+      <img className="avatar" src="./IMG_1362.JPG" alt="profile-pic" />
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1> Jitendra Kumar Pandey</h1>
+      <p> hello</p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="React" emoji="💪" color="blue" />
+      <Skill skill="HTML+CSS" emoji="💪" color="orange" />
+      <Skill skill="JavaScript" emoji="💪" color="yellow" />
+      <Skill skill="Svelte" emoji="💪" color="orangered" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+export default App;
